be8bcffd54aa73072d2806b8148728c7357cecd1
Pravin P Patil <prvnpatil11@gmail.com>
2022-07-12 18:11:00 +0530

    load api created to run job

--- src/main/java/org/pk/springbatchexample/controller/CardTransactionController.java
+++ src/main/java/org/pk/springbatchexample/controller/CardTransactionController.java
@@ -1,6 +1,7 @@
 package org.pk.springbatchexample.controller;
 
 import org.pk.springbatchexample.entity.CardTransaction;
+import org.pk.springbatchexample.processor.FileProcessor;
 import org.pk.springbatchexample.repository.CardTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,16 @@ public class CardTransactionController {
 
     @Autowired
     private CardTransactionRepository repository;
+
+    @Autowired
+    private FileProcessor fileProcessor;
+
+    @GetMapping("/load")
+    public String processCardTransactions() {
+        fileProcessor.setFileName("src/main/resources/electronic-card-transactions-may-2022-csv-tables");
+        boolean response = fileProcessor.processFile();
+        return response ? "OK" : "Something went wrong";
+    }
 
     @GetMapping
     public List<CardTransaction> cardTransactions() {
