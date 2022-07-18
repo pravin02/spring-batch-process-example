fadac76314779e1f5659f5197953eaa409ed0ea5
pravin02 <prvnpatil11@gmail.com>
2022-07-18 20:31:14 +0530

    minor change

--- src/main/java/org/pk/springbatchexample/controller/CardTransactionController.java
+++ src/main/java/org/pk/springbatchexample/controller/CardTransactionController.java
@@ -4,10 +4,12 @@ import org.pk.springbatchexample.entity.CardTransaction;
 import org.pk.springbatchexample.processor.FileProcessor;
 import org.pk.springbatchexample.repository.CardTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
@@ -21,8 +23,8 @@ public class CardTransactionController {
     private FileProcessor fileProcessor;
 
     @GetMapping("/load")
-    public String processCardTransactions() {
-        fileProcessor.setFileName("src/main/resources/electronic-card-transactions-may-2022-csv-tables");
+    public String processCardTransactions() throws FileNotFoundException {
+        fileProcessor.setFileName(new ClassPathResource("electronic-card-transactions-may-2022-csv-tables.csv").getPath());
         boolean response = fileProcessor.processFile();
         return response ? "OK" : "Something went wrong";
     }
