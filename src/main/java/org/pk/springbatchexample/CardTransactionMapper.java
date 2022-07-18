08c5d3b1b548238c0a6ed5fb5d690a787281e272
pravin02 <prvnpatil11@gmail.com>
2022-07-18 20:35:37 +0530

    cognetive sonar fix

--- src/main/java/org/pk/springbatchexample/CardTransactionMapper.java
+++ src/main/java/org/pk/springbatchexample/CardTransactionMapper.java
@@ -16,19 +16,24 @@ public class CardTransactionMapper {
         StringTokenizer stringTokenizer = new StringTokenizer(line, ",");
         for (int i = 0; stringTokenizer.hasMoreTokens(); i++) {
             String token = stringTokenizer.nextToken();
-            if (i == 0) cardTransaction.setSeriesReference(token);
-            if (i == 1) cardTransaction.setPeriod(token);
-            if (i == 2) cardTransaction.setDataValue(token);
-            if (i == 3) cardTransaction.setStatus(token);
-            if (i == 4) cardTransaction.setUnits(token);
-            if (i == 5) cardTransaction.setSubject(token);
-            if (i == 6) cardTransaction.setTransactionGroup(token);
-            if (i == 7) cardTransaction.setSeriesTitle1(token);
-            if (i == 8) cardTransaction.setSeriesTitle2(token);
-            if (i == 9) cardTransaction.setSeriesTitle3(token);
-            if (i == 10) cardTransaction.setSeriesTitle4(token);
-            if (i == 11) cardTransaction.setSeriesTitle5(token);
+            mapTokenToTransaction(i, cardTransaction, token);
         }
         return cardTransaction;
     }
+
+    private void mapTokenToTransaction(int i, CardTransaction cardTransaction, String token) {
+        if (i == 0) cardTransaction.setSeriesReference(token);
+        if (i == 1) cardTransaction.setPeriod(token);
+        if (i == 2) cardTransaction.setDataValue(token);
+        if (i == 3) cardTransaction.setStatus(token);
+        if (i == 4) cardTransaction.setUnits(token);
+        if (i == 5) cardTransaction.setSubject(token);
+        if (i == 6) cardTransaction.setTransactionGroup(token);
+        if (i == 7) cardTransaction.setSeriesTitle1(token);
+        if (i == 8) cardTransaction.setSeriesTitle2(token);
+        if (i == 9) cardTransaction.setSeriesTitle3(token);
+        if (i == 10) cardTransaction.setSeriesTitle4(token);
+        if (i == 11) cardTransaction.setSeriesTitle5(token);
+    }
+
 }
